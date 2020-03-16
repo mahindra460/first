d0f5062b7d7254653e0efb6bc808f8835bc91c56
Mahendra Reddy <mahendrareddy@Mahendras-MacBook-Air.local>
2020-03-16 19:04:04 +0530

    myvc

---
+++ first/first/MyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MyViewController.swift
+//  first
+//
+//  Created by Mahendra Reddy on 16/03/20.
+//  Copyright © 2020 Mahendra Reddy. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hai")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
