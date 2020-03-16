2c3865f56885495bfde457d6ddbe23af0b5805ab
Mahendra Reddy <mahendrareddy@Mahendras-MacBook-Air.local>
2020-03-16 17:47:20 +0530

    Devlop

---
+++ first/first/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Mahendra Reddy on 16/03/20.
+//  Copyright © 2020 Mahendra Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        print("Hello")
+    }
+    
+}
+
